bb2ccd27f1a939ca51aaa29411bc162d2725108a
Abdelrahman Essam <abdoo2083001@gmail.com>
2022-06-03 22:17:34 +0200

    initialize project

---
+++ petClinic_soringProject/src/main/java/horus/petclinic_soringproject/PetClinicSoringProjectApplication.java
@@ -0,0 +1,13 @@
+package horus.petclinic_soringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicSoringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicSoringProjectApplication.class, args);
+    }
+
+}
---
+++ petClinic_soringProject/src/test/java/horus/petclinic_soringproject/PetClinicSoringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package horus.petclinic_soringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicSoringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
